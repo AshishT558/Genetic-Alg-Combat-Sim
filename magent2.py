7ea1667c23fd266502a650c590930b4d4657b9a6
kilpy <b.elbridgegarey@gmail.com>
2024-12-18 17:41:20 -0500

    Creation of MAgent2 branch

---
+++ magent2.py
@@ -0,0 +1,16 @@
+# Goals: 
+# 1. Generate 2 types of agents for each side:
+    # 1 from start, 1 from end stats
+# 2. Train them (set rewards rules in RL)
+    # Stats:
+        # 1. Health as top contender energy
+        # 2. Damage as weights
+    # Behavior:
+        # 1. Aggressiveness = higher reward for attacking opponents than food
+        # 2. Resourcefulness = higher reward from gathering food than attacking
+            # Normalize according to percentage?
+
+# 3. Observe a gather.py battle between them 
+# 3a. Observe a gather.py battle between them and the original agents (?)
+
+# 4. Adjust rewards rules as needed. 
