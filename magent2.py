f2b5df49e7dde237cd21203664bebe1d365e418a
kilpy <b.elbridgegarey@gmail.com>
2024-12-19 23:45:20 -0500

    Update magent2.py

--- magent2.py
+++ magent2.py
@@ -20,3 +20,36 @@
 # Imports
 ############################################################################################################
 
+import magent2
+
+
+
+############################################################################################################
+# Environment Config 
+############################################################################################################
+
+# env = make_env(raw_env)
+
+'''
+options = {
+        "width": 1,
+        "length": 1,
+        "hp": 10,
+        "speed": 1,
+        "view_range": gw.CircleRange(6),
+        "attack_range": gw.CircleRange(1),
+        "damage": 2,
+        "step_recover": 0.1,
+        "attack_in_group": True,
+        "step_reward": step_reward,
+        "dead_penalty": dead_penalty,
+        "attack_penalty": attack_penalty,
+    }
+
+    
+agent = cfg.register_agent_type(name="agent", attr=options)
+
+
+get_config()
+'''
+
